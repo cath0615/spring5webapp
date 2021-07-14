7833914e9c21aae931431a4615626e37c8974f47
Josh <josh@Joshs-MacBook-Pro.local>
2021-07-14 17:11:14 +0800

    add two new DOJO

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,47 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+//using this component annotation to tell the spring framework that this is a spring managed component
+public class BootStrapData implements CommandLineRunner {
+    //interface commandlinerunner which has the one method that we need to bring in(the run method) which
+    //takes a string of arguments(we are not using). The spring managed component has two properties:
+    //authorRepository and bookRepository and we initialize them in the constructor which tells the
+    //spring framework to when it constructs this bin that it has to inject and instance of author
+    //repository and book repository
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthor().add(eric);
+// Spring data JPA is utilizing Hibernate to save these data in memory H2 database
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthor().add(rod);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+    }
+}
